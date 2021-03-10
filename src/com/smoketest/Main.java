ae6b14f4617d152f2b1d6649591cc23a25c42186
yhoard1 <yusef.hoard@improving.com>
2021-03-10 09:50:39 -0500

    Added time outs between URLS

--- src/com/smoketest/Main.java
+++ src/com/smoketest/Main.java
@@ -10,11 +10,11 @@ public class Main {
 
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\yhoar\\OneDrive\\Desktop\\SeleniumDownloads\\Drivers\\chromedriver.exe");
         WebDriver obj = new ChromeDriver();
-        Thread.sleep(5000);
+
 
 // Launch Cypress
         obj.get("https://www.commissionpaymentservices.com/login.aspx?id=ctl");
-
+        Thread.sleep(5000);
         if(obj.getPageSource().contains("Welcome"))
         {
             System.out.println("Cypress ----> PASSED");
@@ -37,7 +37,7 @@ public class Main {
         }
 // Launch FL Hwy Safety
         obj.get("https://admin.fhpcvepayments.com/");
-
+        Thread.sleep(5000);
         if(obj.getPageSource().contains("Please enter your ID and password"))
         {
             System.out.println("FL Hwy Safety ----> PASSED");
@@ -48,7 +48,7 @@ public class Main {
         }
 // Launch Meridian S Jersey Gas
         obj.get("http://meridian-bill2pay.southjerseygas.com");
-
+        Thread.sleep(5000);
         if(obj.getPageSource().contains("Meridian Payment Landing Site"))
         {
             System.out.println("Meridian S Jersey Gas ----> PASSED");
@@ -59,7 +59,7 @@ public class Main {
         }
 // Launch Meridian Etown Gas
         obj.get("https://meridian-bill2pay.elizabethtowngas.com/");
-
+        Thread.sleep(5000);
         if(obj.getPageSource().contains("Meridian Payment Landing Site"))
         {
             System.out.println("Meridian Etown Gas ----> PASSED");
@@ -70,7 +70,7 @@ public class Main {
         }
 // Launch Bill2Pay
         obj.get("https://pay.bill2pay.com/");
-
+        Thread.sleep(5000);
         if(obj.getPageSource().contains("Missing or Invalid Client Information"))
         {
             System.out.println("Bill2Pay ----> PASSED");
@@ -81,7 +81,7 @@ public class Main {
         }
 // Launch PeachCare for Kids
         obj.get("https://peachcaremypayments.bill2pay.com/login/");
-
+        Thread.sleep(5000);
         if(obj.getPageSource().contains("Don't have a profile?"))
         {
             System.out.println("PeachCare for Kids ----> PASSED");
@@ -92,7 +92,7 @@ public class Main {
         }
 // Launch PeachCare Express
         obj.get("https://peachcare.bill2pay.com/");
-
+        Thread.sleep(5000);
         if(obj.getPageSource().contains("Case Number"))
         {
             System.out.println("PeachCare Express ----> PASSED");
@@ -103,7 +103,7 @@ public class Main {
         }
 // Launch B2P API
         obj.get("https://api2.bill2pay.com/b2pclassicapi.ashx");
-
+        Thread.sleep(5000);
         if(obj.getPageSource().contains("ACTIVE"))
         {
             System.out.println("B2P API ----> PASSED");
@@ -114,7 +114,7 @@ public class Main {
         }
 // Launch B2P web services
         obj.get("https://webservices.bill2pay.com/sso/gateway.ashx");
-
+        Thread.sleep(5000);
         if(obj.getPageSource().contains("ACTIVE"))
         {
             System.out.println("B2P web services ----> PASSED");
@@ -125,7 +125,7 @@ public class Main {
         }
 // Launch B2P Auto agent
         obj.get("https://autoagent.bill2pay.com/pay/");
-
+        Thread.sleep(5000);
         if(obj.getPageSource().contains("Credit/Debit Card Number"))
         {
             System.out.println("B2P Auto agent ----> PASSED");
@@ -136,7 +136,7 @@ public class Main {
         }
 // Launch B2P Collector
         obj.get("https://collectorfl.bill2pay.com/default.aspx?token=b2e4c6c88441464390e70faec3e3e892");
-
+        Thread.sleep(5000);
         if(obj.getPageSource().contains("Payment Options"))
         {
             System.out.println("B2P Collector ----> PASSED");
@@ -147,7 +147,7 @@ public class Main {
         }
 // Launch B2P Client portal
         obj.get("https://clientportal.bill2pay.com/login/login.aspx");
-
+        Thread.sleep(5000);
         if(obj.getPageSource().contains("Secure Login Page"))
         {
             System.out.println("B2P Client portal ----> PASSED");
